da9d0dd77f55f8c69b8de718ec8a0396318840be
laqinfan <lfan1@memphis.edu>
2018-04-10 21:59:54 +0000

    update the homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410021513) do
+ActiveRecord::Schema.define(version: 20180410160144) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "street1"
@@ -36,6 +36,18 @@ ActiveRecord::Schema.define(version: 20180410021513) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
+  create_table "contracts", force: :cascade do |t|
+    t.string "start_date"
+    t.string "return_date"
+    t.float "price"
+    t.float "subtotal"
+    t.float "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+    t.string "string"
   end
 
   create_table "locations", force: :cascade do |t|
