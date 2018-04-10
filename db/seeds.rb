10f5051c19b41866de165551284280618630a5b2
Katie <kbridson@memphis.edu>
2018-04-10 02:08:52 +0000

    Add few additional seeds. Basic forms complete but still need styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,10 @@ place2 = Address.create!(street1: "5678 Bored Lane", street2: "", city: "Memphis
 place3 = Address.create!(street1: "1234 Fleet St", street2: "", city: "Memphis", state: "TN", zipcode: 38016)
 place4 = Address.create!(street1: "6543 Carsick Lane", street2: "", city: "Memphis", state: "TN", zipcode: 38016)
 place5 = Address.create!(street1: "4567 Magnolia Place", street2: "", city: "Memphis", state: "TN", zipcode: 38016)
+place6 = Address.create!(street1: "1789 Avenue B", street2: "", city: "Memphis", state: "SC", zipcode: 38120)
+place7 = Address.create!(street1: "1456 MyStreet St", street2: "", city: "Cordova", state: "TN", zipcode: 38016)
+place8 = Address.create!(street1: "3459 Carson Lane", street2: "", city: "Memphis", state: "TN", zipcode: 38065)
+
 
 user1 = User.create!(email: "test1@test.com", password: "password")
 user2 = User.create!(email: "test2@test.com", password: "password")
@@ -20,13 +24,24 @@ user5 = User.create!(email: "test5@test.com", password: "password")
 
 car1 = user1.cars.create!(make: "Toyota", model: "Camry", year: 2017, color: "white", description: "Hello", category: "Sedan", mileage: 34500, number_of_seats: 4, price_per_day: 19.567)
 car1.locations.create!(address: place1, status: "saved")
-car1.locations.create!(address: place2, status: "current")
+car1.locations.create!(address: place2, status: "saved")
+car1.locations.create!(address: place8, status: "current")
 car1.save!
 
 car2 = user1.cars.create!(make: "Ford", model: "Taurus", year: 2014, color: "grey", description: "Hello", category: "Sedan", mileage: 34567, number_of_seats: 4, price_per_day: 21.99)
-car2.locations.create!(address: place1, status: "current")
+car2.locations.create!(address: place1, status: "saved")
+car2.locations.create!(address: place4, status: "current")
 car2.save!
 
 car3 = user2.cars.create!(make: "Dodge", model: "Charger", year: 2014, color: "black", description: "Hello", category: "Sports Car", mileage: 45000, number_of_seats: 4, price_per_day: 55.99)
 car3.locations.create!(address: place3, status: "current")
 car3.save!
+
+car4 = user1.cars.create!(make: "Subaru", model: "Outback", year: 2015, color: "red", description: "This car is great!", category: "SUV", mileage: 56700, number_of_seats: 4, price_per_day: 34.99)
+car4.locations.create!(address: place6, status: "current")
+car4.locations.create!(address: place5, status: "saved")
+car4.save!
+
+car5 = user1.cars.create!(make: "BMW", model: "X5", year: 2017, color: "black", description: "This is a very nice car!", category: "Luxury Car", mileage: 34000, number_of_seats: 4, price_per_day: 75.99)
+car5.locations.create!(address: place7, status: "current")
+car5.save!
